52e63578f87b340e2e44c9ea998d3593a1c6e1a3
kienguyen01 <kienguyen01@gmail.com>
2022-12-04 14:49:55 +0100

    structure done

--- src/components/Companies/CreateCompany.vue
+++ src/components/Companies/CreateCompany.vue
@@ -1,16 +1,50 @@
 <template>
-    <section>
-      <div class="container">
-        <h2 class="mt-3 mt-lg-5">Welcome to the CreateCompany</h2>
-      </div>
-    </section>
-  </template>
-  
-  <script>
-  export default {
-    name: "CreateCompany",
-  };
-  </script>
-  
-  <style>
-  </style>
+  <section>
+    <div class="container">
+      <form ref="form">
+        <h2 class="mt-3 mt-lg-5">Create Company</h2>
+        <h5 class="mb-4"></h5>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Name</span>
+          <input type="text" class="form-control" v-model="company.Name" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Last Name</span>
+          <input type="text" class="form-control" v-model="company.Location" />
+        </div>
+
+        <div class="input-group mt-4">
+          <button type="button" class="btn btn-primary">
+            Save changes
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger"
+            @click="this.$router.push('/CompanyList')"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "CreateCompany",
+  data() {
+    return {
+      product: {
+        Name: "",
+        Location: "",
+      },
+    };
+  }  
+};
+</script>
+
+<style>
+</style>
--- src/components/Users/EditUser.vue
+++ src/components/Users/EditUser.vue
@@ -1,7 +1,43 @@
 <template>
     <section>
       <div class="container">
-        <h2 class="mt-3 mt-lg-5">Welcome to the EditUser</h2>
+        <form ref="form">
+          <h2 class="mt-3 mt-lg-5">Edit User</h2>
+          <h5 class="mb-4"></h5>
+  
+          <div class="input-group mb-3">
+            <span class="input-group-text">First Name</span>
+            <input type="text" class="form-control" v-model="user.Firstname" />
+          </div>
+  
+          <div class="input-group mb-3">
+            <span class="input-group-text">Last Name</span>
+            <input type="text" class="form-control" v-model="user.Lastname" />
+          </div>
+  
+          <div class="input-group mb-3">
+            <span class="input-group-text">Email</span>
+            <input type="text" class="form-control" v-model="user.Email" />
+          </div>
+  
+          <div class="input-group mb-3">
+            <span class="input-group-text">Role</span>
+            <input type="text" class="form-control" v-model="user.Role" />
+          </div>
+
+          <div class="input-group mt-4">
+            <button type="button" class="btn btn-primary">
+              Save changes
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger"
+              @click="this.$router.push('/products')"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   </template>
@@ -9,6 +45,20 @@
   <script>
   export default {
     name: "EditUser",
+    props: {
+      id: Guid,
+    },
+    data() {
+      return {
+        product: {
+          Firstname: "",
+          Lastname: "",
+          Email: "",
+          Role: "",
+        },
+        categories: [],
+      };
+    }  
   };
   </script>
   
